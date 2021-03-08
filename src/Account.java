938b5b8a41dfbc84d2945bffbc63512b050144ee
brunozzz <16675056030>
2021-03-08 16:26:36 +0800

    none

---
+++ src/Account.java
@@ -0,0 +1,19 @@
+import acm.graphics.GImage;
+
+public class Account {
+    String name;
+    String status;
+    GImage avatar;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public GImage getAvatar() {
+        return avatar;
+    }
+}
